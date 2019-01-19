de31c2aeb6e01a202e4e009859939a82524ba932
Mateus Gonçalo <mateusgon57@gmail.com>
2019-01-19 15:42:16 -0200

    Adicionando o model Pessoa e o enum PessoaSituacao

---
+++ Desafio2IClips/Models/Enums/PessoaSituacao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Desafio2IClips.Models.Enums
+{
+    public enum PessoaSituacao
+    {
+        EmAndamento = 0,
+        Atrasado = 1
+    }
+}
---
+++ Desafio2IClips/Models/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using Desafio2IClips.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Desafio2IClips.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public PessoaSituacao Situacao { get; set; }
+    }
+}
